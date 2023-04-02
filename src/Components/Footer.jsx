5b0a43368281a59086c7c1edd199e18b4f8e0050
Ebuka099 <110292112+Codebadger99@users.noreply.github.com>
2023-04-02 14:57:20 -0700

    Initial commit

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,12 @@
+import { Box, Text, Flex } from "@chakra-ui/react";
+
+const Footer = () => {
+  return (
+    <Box marginTop={50}>
+      <Flex justifyContent="center" alignItems="center">
+        <Text>Powered By Open AI</Text>
+      </Flex>
+    </Box>
+  );
+};
+export default Footer;
